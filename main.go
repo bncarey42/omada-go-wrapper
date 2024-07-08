3f78300823a2a67947500d676f6c73e7cb6f9de0
Ben Carey <bncarey42@users.noreply.github.com>
2024-07-07 22:49:56 -0500

    WIP

--- main.go
+++ main.go
@@ -1,53 +1,22 @@
 package main
 
 import (
-	"crypto/tls"
 	"fmt"
-	"io"
-	"net/http"
-)
+	"os"
 
-const (
-	host     = "https://noosphere-ctrl:8043/api"
-	user     = "omada-tf-test"
-	password = "V8qMh%k%J*xq&87aco"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	// CAL API
+	godotenv.Load()
 
-	//	requestURL := fmt.Sprintf("%s/v2/%s", host, "login")
-	//	fmt.Printf("ATTEMPTING TO LOG IN\n")
-	//	jsonBody := []byte(fmt.Sprintf(`{"username": %s, "password": %s}`, user, password))
-	//	bodyReader := bytes.NewReader(jsonBody)
-	//
-	//	req, err := http.NewRequest(http.MethodPost, requestURL, bodyReader)
-	//	if err != nil {
-	//		fmt.Printf(fmt.Sprintf("ERROR 1::: %s", err.Error()))
-	//		os.Exit(1)
-	//	}
+	host_name := os.Getenv("OMADA_HOST")
+	port := os.Getenv("OMADA_PORT")
+	apiEndpoint := os.Getenv("OMADA_BASE_ENDPOINT")
 
-	// req.Header.Add("Content-Type", "application/json")
+	sitename := os.Getenv("OMADA_SITENAME")
 
-	//	resp, err := client.Do(req)
-	//	if err != nil {
-	//		fmt.Printf(fmt.Sprintf("ERROR 2::: %s", err.Error()))
-	//		os.Exit(1)
-	//	}
-	//	defer func() { _ = resp.Body.Close() }()
-	//	ret_body, _ := io.ReadAll(resp.Body)
-	//	fmt.Printf("%s", ret_body)
+	omadaClient := NewOmadaClient(fmt.Sprintf("https://%s:%s/%s", host_name, port, apiEndpoint), sitename)
 
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	client := &http.Client{Transport: tr}
-
-	r, err := client.Get(fmt.Sprintf("%s/info", host))
-	if err != nil {
-		panic(err)
-	}
-	defer func() { _ = r.Body.Close() }()
-	body, _ := io.ReadAll(r.Body)
-	fmt.Printf("%s", body)
+	omadaClient.Login(os.Getenv("OMADA_USER"), os.Getenv("OMADA_PASSWORD"))
 }
--- omadaClient.go
+++ omadaClient.go
@@ -13,24 +13,26 @@ import (
 type OmadaClient struct {
 	Host       string
 	Site       string
-	OmadacId   string
+	ApiInfo    *ApinInfoResult
 	HTTPClient *http.Client
 }
 
 type ApiInfo struct {
-	ErrorCode int    `json:"errorCode"`
-	Msg       string `json:"msg"`
-	Result    struct {
-		ControllerVer  string `json:"controllerVer"`
-		APIVer         string `json:"apiVer"`
-		Configured     bool   `json:"configured"`
-		Type           int    `json:"type"`
-		SupportApp     bool   `json:"supportApp"`
-		OmadacID       string `json:"omadacId"`
-		RegisteredRoot bool   `json:"registeredRoot"`
-		OmadacCategory string `json:"omadacCategory"`
-		MspMode        bool   `json:"mspMode"`
-	} `json:"result"`
+	ErrorCode int            `json:"errorCode"`
+	Msg       string         `json:"msg"`
+	Result    ApinInfoResult `json:"result"`
+}
+
+type ApinInfoResult struct {
+	ControllerVer  string `json:"controllerVer"`
+	APIVer         string `json:"apiVer"`
+	Configured     bool   `json:"configured"`
+	Type           int    `json:"type"`
+	SupportApp     bool   `json:"supportApp"`
+	OmadacID       string `json:"omadacId"`
+	RegisteredRoot bool   `json:"registeredRoot"`
+	OmadacCategory string `json:"omadacCategory"`
+	MspMode        bool   `json:"mspMode"`
 }
 
 type ClientToken struct {
@@ -63,21 +65,41 @@ func NewOmadaClient(host string, site string) *OmadaClient {
 	return client
 }
 
-func (c OmadaClient) ApiInfo() {
+func (c OmadaClient) BuildURL(slug string) (string, error) {
+	if c.ApiInfo == nil {
+		apiInfo, err := c.GetApiInfo()
+		if err != nil {
+			return "", err
+		}
+		c.ApiInfo = &apiInfo.Result
+	}
+
+	return fmt.Sprintf("%s/v%d/%s/%s", c.Host, c.ApiInfo.APIVer, c.ApiInfo.OmadacID, slug), nil
+}
+
+func (c OmadaClient) GetApiInfo() (*ApiInfo, error) {
+	var apiInfo ApiInfo
 	ret, err := c.HTTPClient.Get(fmt.Sprintf("%s/info", c.Host))
 	if err != nil {
-		panic(err)
+		log.Fatalf("Unable to get apiInfo due to %s", err.Error())
+		return nil, err
 	}
 	defer func() { _ = ret.Body.Close() }()
 	body, _ := io.ReadAll(ret.Body)
-	var apiInfo ApiInfo
 	err = json.Unmarshal(body, &apiInfo)
 	if err != nil {
 		log.Fatalf("Unable to marshal JSON due to %s", err)
+		return nil, err
 	}
-	c.OmadacId = apiInfo.Result.OmadacID
+	return &apiInfo, nil
 }
 
-func (c OmadaClient) Login(user string, password string) *ClientToken {
-	return &ClientToken{}
+func (c OmadaClient) Login(user string, password string) (*ClientToken, error) {
+	url, err := c.BuildURL("login")
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("login url::: %s", url)
+	return nil, nil
 }
